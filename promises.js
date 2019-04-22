67ac36b528f3fca91a2c66cfdfddc335bd3cbc94
Rob Hess <robwhess@gmail.com>
2019-04-22 15:47:39 -0700

    Add code from 4/22 lecture.

--- promises.js
+++ promises.js
@@ -1,18 +1,71 @@
 function waitAndEcho(message, callback) {
   setTimeout(() => {
-    callback(message);
+    if (message === 'generate error') {
+      callback({
+        error: `we didn't like this message: "${message}"`
+      }, null);
+    } else {
+      callback(null, message);
+    }
   }, 1000);
 }
 
-waitAndEcho("3", (data) => {
-  console.log(data);
-  waitAndEcho("2", (data) => {
-    console.log(data);
-    waitAndEcho("1", (data) => {
-      console.log(data);
-      waitAndEcho("... blastoff", (data) => {
-        console.log(data);
-      });
-    });
+// waitAndEcho("3", (err, data) => {
+//   if (err) {
+//     console.error("Error:", err);
+//   } else {
+//     console.log(data);
+//     waitAndEcho("2", (err, data) => {
+//       if (err) {
+//         console.error("Error:", err);
+//       } else {
+//         console.log(data);
+//         waitAndEcho("generate error", (err, data) => {
+//           if (err) {
+//             console.error("Error:", err);
+//           } else {
+//             console.log(data);
+//             waitAndEcho("... blastoff", (err, data) => {
+//               if (err) {
+//                 console.error("Error:", err);
+//               } else {
+//                 console.log(data);
+//               }
+//             });
+//           }
+//         });
+//       }
+//     });
+//   }
+// });
+
+function waitAndEchoWithPromise(message) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (message === 'generate error') {
+        reject({
+          error: `we didn't like this message: "${message}"`
+        });
+      } else {
+        resolve(message);
+      }
+    }, 1000);
   });
-});
+}
+
+waitAndEchoWithPromise("3")
+  .then((data) => {
+    console.log(data);
+    return waitAndEchoWithPromise("2");
+  })
+  .then((data) => {
+    console.log(data);
+    return waitAndEchoWithPromise("1");
+  })
+  .then((data) => {
+    console.log(data);
+    return waitAndEchoWithPromise("... blastoff");
+  })
+  .then((data) => {
+    console.log(data);
+  });
